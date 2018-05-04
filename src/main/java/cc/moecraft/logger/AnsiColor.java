c697af4a68ab056e388a3996d2bdc24ab034d724
Hykilpikonna <admin@moecraft.cc>
2018-05-04 19:32:12 -0400

    Release 1.0

---
+++ src/main/java/cc/moecraft/logger/AnsiColor.java
@@ -0,0 +1,54 @@
+package cc.moecraft.logger;
+
+import static cc.moecraft.logger.AnsiConsants.PREFIX;
+import static cc.moecraft.logger.AnsiConsants.SUFFIX;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/04 创建!
+ * Created by Hykilpikonna on 2018/05/04!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public enum AnsiColor
+{
+    RESET(0),
+
+    BLACK(30),
+    RED(31),
+    GREEN(32),
+    YELLOW(33),
+    BLUE(34),
+    PURPLE(35),
+    CYAN(36),
+    WHITE(37);
+
+    int code;
+
+    AnsiColor(int code)
+    {
+        this.code = code;
+    }
+
+    @Override
+    public String toString()
+    {
+        return PREFIX + code + SUFFIX;
+    }
+
+    public String get()
+    {
+        return toString();
+    }
+
+    public String getBright()
+    {
+        return PREFIX + code + ";1" + SUFFIX;
+    }
+
+    public String getBackground()
+    {
+        return PREFIX + (code + 10) + SUFFIX;
+    }
+}
---
+++ src/test/java/LoggerTest.java
@@ -0,0 +1,22 @@
+import cc.moecraft.logger.AnsiColor;
+import cc.moecraft.logger.AnsiFormat;
+import cc.moecraft.logger.DebugLogger;
+import com.sun.org.apache.xerces.internal.dom.AttrNSImpl;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/05/04 创建!
+ * Created by Hykilpikonna on 2018/05/04!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class LoggerTest
+{
+    private static DebugLogger logger = new DebugLogger("LoggerTest", true);
+
+    public static void main(String[] args)
+    {
+        logger.debug(AnsiColor.RED.getBright() + AnsiColor.BLUE.getBackground() + AnsiFormat.HIGH_INTENSITY + " Hi ");
+    }
+}
