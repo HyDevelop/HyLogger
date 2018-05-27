17247783339069f9412245e7b18c61822696d172
Hykilpikonna <admin@moecraft.cc>
2018-05-27 10:25:24 -0400

    [+] Log写入文件

--- src/test/java/LoggerTest.java
+++ src/test/java/LoggerTest.java
@@ -3,6 +3,8 @@ import cc.moecraft.logger.AnsiFormat;
 import cc.moecraft.logger.DebugLogger;
 import com.sun.org.apache.xerces.internal.dom.AttrNSImpl;
 
+import java.io.File;
+
 /**
  * 此类由 Hykilpikonna 在 2018/05/04 创建!
  * Created by Hykilpikonna on 2018/05/04!
@@ -13,7 +15,7 @@ import com.sun.org.apache.xerces.internal.dom.AttrNSImpl;
  */
 public class LoggerTest
 {
-    private static DebugLogger logger = new DebugLogger("LoggerTest", true);
+    private static DebugLogger logger = new DebugLogger("LoggerTest", true, "logs", "log");
 
     public static void main(String[] args)
     {
