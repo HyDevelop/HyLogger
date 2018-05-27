6ba92d36e40fd53cfd70480dc052dcbeac29782f
Hykilpikonna <admin@moecraft.cc>
2018-05-27 10:36:35 -0400

    [+] 自动保存

--- src/test/java/LoggerTest.java
+++ src/test/java/LoggerTest.java
@@ -17,10 +17,13 @@ public class LoggerTest
 {
     private static DebugLogger logger = new DebugLogger("LoggerTest", true, "logs", "log");
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws InterruptedException
     {
         logger.log("一条测试Log消息");
         logger.debug("一条测试Debug消息");
         logger.error("一条测试Error消息");
+        Thread.sleep(200);
+        int i = 1 / 0;
+        logger.error("执行到了报错后的");
     }
 }
