14d4677855829a1835e89c93b8ab099306d721cf
Hykilpikonna <admin@moecraft.cc>
2018-07-04 15:00:34 +0800

    [+] 添加日志实例管理器

---
+++ src/main/java/cc/moecraft/logger/LoggerInstanceManager.java
@@ -0,0 +1,34 @@
+package cc.moecraft.logger;
+
+import cc.moecraft.logger.environments.LogEnvironment;
+import lombok.Getter;
+
+import java.util.*;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/07/04 创建!
+ * Created by Hykilpikonna on 2018/07/04!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class LoggerInstanceManager
+{
+    @Getter
+    private Map<String, HyLogger> cachedInstance = new HashMap<>();
+
+    @Getter
+    private List<LogEnvironment> environments;
+
+    public LoggerInstanceManager(LogEnvironment... environments)
+    {
+        this.environments = Arrays.asList(environments);
+    }
+
+    public LoggerInstanceManager addEnvironment(LogEnvironment ... environments)
+    {
+        this.environments.addAll(Arrays.asList(environments));
+        return this;
+    }
+}
