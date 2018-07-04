72bbb300ba9a6c93db8da7e27f6c34262dd9dd88
Hykilpikonna <admin@moecraft.cc>
2018-07-04 14:59:23 +0800

    [+] 带颜色的文件日志环境

---
+++ src/main/java/cc/moecraft/logger/environments/FileColoredEnv.java
@@ -0,0 +1,36 @@
+package cc.moecraft.logger.environments;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/07/04 创建!
+ * Created by Hykilpikonna on 2018/07/04!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class FileColoredEnv extends FileEnv
+{
+    public FileColoredEnv(File file) throws IOException
+    {
+        super(file);
+    }
+
+    public FileColoredEnv(String filePath, String fileName) throws IOException
+    {
+        super(filePath, fileName);
+    }
+
+    @Override
+    public void logRaw(String message)
+    {
+        getFileWriter().write(message + "\n");
+        if (System.currentTimeMillis() - lastLogTime > 5000)
+        {
+            lastLogTime = System.currentTimeMillis();
+            getFileWriter().flush();
+        }
+    }
+}
