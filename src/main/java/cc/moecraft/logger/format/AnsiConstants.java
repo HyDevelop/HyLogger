43de00c0320813ad7b581777ae2de1780ca0d4fd
Hykilpikonna <admin@moecraft.cc>
2018-07-04 14:54:47 +0800

    [M] 移动包名

--- src/main/java/cc/moecraft/logger/format/AnsiConstants.java
+++ src/main/java/cc/moecraft/logger/format/AnsiConstants.java
@@ -1,4 +1,6 @@
-package cc.moecraft.logger;
+package cc.moecraft.logger.format;
+
+import java.util.ArrayList;
 
 /**
  * 此类由 Hykilpikonna 在 2018/05/04 创建!
@@ -8,7 +10,7 @@ package cc.moecraft.logger;
  *
  * @author Hykilpikonna
  */
-public class AnsiConsants
+public class AnsiConstants
 {
     public static final String PREFIX = System.getProperty("os.name").toLowerCase().contains("win") ? "\033[0;" : "\u001B[";
     public static final String SUFFIX = "m";
