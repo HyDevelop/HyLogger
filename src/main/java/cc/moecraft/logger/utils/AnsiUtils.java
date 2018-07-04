4b5b1d0543c9b84a381a3126a2db170e8813b3fc
Hykilpikonna <admin@moecraft.cc>
2018-07-04 14:57:57 +0800

    [+] 添加ANSI工具类

---
+++ src/main/java/cc/moecraft/logger/utils/AnsiUtils.java
@@ -0,0 +1,17 @@
+package cc.moecraft.logger.utils;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/07/04 创建!
+ * Created by Hykilpikonna on 2018/07/04!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class AnsiUtils
+{
+    public static String removeFormat(String original)
+    {
+        return original.replaceAll("\u001B\\[[;\\d]*m", "");
+    }
+}
