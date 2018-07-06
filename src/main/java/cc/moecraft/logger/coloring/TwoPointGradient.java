c2b43e3cb39ac93fd44e683e2dd007334acf7d47
Hykilpikonna <admin@moecraft.cc>
2018-07-06 16:19:33 +0800

    [+] 点到点线性渐变类

---
+++ src/main/java/cc/moecraft/logger/coloring/TwoPointGradient.java
@@ -0,0 +1,21 @@
+package cc.moecraft.logger.coloring;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.awt.*;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/07/05 创建!
+ * Created by Hykilpikonna on 2018/07/05!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+@Data @AllArgsConstructor
+public class TwoPointGradient
+{
+    private AnsiRGB color1; // 从什么颜色开始渐变
+    private AnsiRGB color2; // 渐变到什么颜色
+}
