b1ce7004445457de18febd621b86647b2f9f0b1c
Hykilpikonna <admin@moecraft.cc>
2018-07-06 16:21:00 +0800

    [+] 用分割的非自然渐变获取渐变文字的工具类

---
+++ src/main/java/cc/moecraft/logger/utils/TextColoringUtil.java
@@ -0,0 +1,31 @@
+package cc.moecraft.logger.utils;
+
+import cc.moecraft.logger.coloring.TwoPointGradient;
+
+import java.awt.*;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/07/05 创建!
+ * Created by Hykilpikonna on 2018/07/05!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class TextColoringUtil
+{
+    public static String getGradientText(String text, Color color1, Color color2)
+    {
+        char[] chars = text.toCharArray();
+        Color[] colors = new TwoPointGradient(color1, color2).getColors(chars.length);
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < chars.length; i++)
+        {
+            result.append(colors[i].toString()).append(chars[i]);
+        }
+
+        return result.toString();
+    }
+}
