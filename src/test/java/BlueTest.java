357d113244659ddcdc76d328f5ddd73b4e4bb23c
Hykilpikonna <admin@moecraft.cc>
2018-07-06 16:16:48 +0800

    [T] 测试蓝色

---
+++ src/test/java/BlueTest.java
@@ -0,0 +1,18 @@
+/**
+ * 此类由 Hykilpikonna 在 2018/05/04 创建!
+ * Created by Hykilpikonna on 2018/05/04!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class BlueTest
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < 256; i += 1)
+        {
+            System.out.print(String.format(" \u001b[38;2;0;0;%sm %s \u001b[0m ", i, i));
+        }
+    }
+}
