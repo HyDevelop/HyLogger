bc512820702c5fbfb22bff0849904d397abd5745
Hykilpikonna <admin@moecraft.cc>
2018-07-06 16:14:18 +0800

    [+] 添加ANSI RGB类

---
+++ src/main/java/cc/moecraft/logger/coloring/AnsiRGB.java
@@ -0,0 +1,17 @@
+package cc.moecraft.logger.coloring;
+
+import cc.moecraft.logger.format.AnsiConstants;
+
+import java.awt.*;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/07/05 创建!
+ * Created by Hykilpikonna on 2018/07/05!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class AnsiRGB extends Color
+{
+}
