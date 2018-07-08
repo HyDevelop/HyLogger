1b93df5d4775aead76ecb6d9b4d5c0f646efadc9
Hykilpikonna <admin@moecraft.cc>
2018-07-08 15:04:45 +0800

    [+] 把一个相对数值的列表转换为绝对数值的列表

--- src/main/java/cc/moecraft/logger/coloring/MultiPointLinearGradient.java
+++ src/main/java/cc/moecraft/logger/coloring/MultiPointLinearGradient.java
@@ -20,5 +20,38 @@ public class MultiPointLinearGradient
     public MultiPointLinearGradient(GradientPoint color1, GradientPoint color2, ArrayList<GradientPoint> colors)
     {
         this.colors = colors;
+
+    /**
+     * 把一个相对数值的列表转换为绝对数值的列表
+     *
+     * 例子:
+     *   100, 红
+     *   100, 蓝
+     *   100, 橙
+     *   50, 紫
+     *
+     * 转换后:
+     *   0, 红
+     *   100, 蓝
+     *   200, 橙
+     *   250, 紫
+     *
+     * @param gradientPoints 相对数值的列表
+     * @return 绝对数值的列表
+     */
+    public static ArrayList<Map.Entry<Integer, GradientPoint>> mapSizes(List<GradientPoint> gradientPoints)
+    {
+        ArrayList<Map.Entry<Integer, GradientPoint>> result = new ArrayList<>();
+
+        int total = 0 - gradientPoints.get(0).amount;
+
+        for (GradientPoint gradientPoint : gradientPoints)
+        {
+            total += gradientPoint.amount;
+            result.add(new HashMap.SimpleEntry<>(total, gradientPoint));
+        }
+
+        return result;
+    }
     }
 }
