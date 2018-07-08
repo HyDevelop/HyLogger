e582d79e38df6be9c9a9febcad7157a0a598ec9f
Hykilpikonna <admin@moecraft.cc>
2018-07-08 15:09:55 +0800

    [T] 测试辅助方法

---
+++ src/test/java/MultiPointLinearGradientTest.java
@@ -0,0 +1,39 @@
+import cc.moecraft.logger.coloring.MultiPointLinearGradient;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 此类由 Hykilpikonna 在 18-7-7 创建!
+ * Created by Hykilpikonna on 18-7-7!
+ * Twitter: @Hykilpikonna
+ * QQ/Wechat: 
+ */
+class MultiPointLinearGradientTest
+{
+    @Test
+    void testMapping()
+    {
+        List<MultiPointLinearGradient.GradientPoint> list = new ArrayList<>();
+
+        list.add(new MultiPointLinearGradient.GradientPoint(Color.RED));
+        list.add(new MultiPointLinearGradient.GradientPoint(Color.BLUE));
+        list.add(new MultiPointLinearGradient.GradientPoint(Color.ORANGE));
+        list.add(new MultiPointLinearGradient.GradientPoint(Color.PINK, 50));
+
+        List<Map.Entry<Integer, MultiPointLinearGradient.GradientPoint>> mappedSizes = MultiPointLinearGradient.mapSizes(list);
+
+        System.out.println(mappedSizes);
+
+        List<Map.Entry<Integer, MultiPointLinearGradient.GradientPoint>> scaledSizes = MultiPointLinearGradient.scaleSizes(mappedSizes, 7);
+
+        System.out.println(scaledSizes);
+
+        List<Map.Entry<Integer, MultiPointLinearGradient.GradientPoint>> nearestTwo = MultiPointLinearGradient.getNearestTwoColors(scaledSizes, 3);
+
+        System.out.println(nearestTwo);
+    }
+}
