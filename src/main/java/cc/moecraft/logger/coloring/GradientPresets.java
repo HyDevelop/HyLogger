48262be867c77da1c382f31cdf48db7ea9cd2bf0
Hykilpikonna <admin@moecraft.cc>
2018-07-08 15:08:57 +0800

    [+] 彩虹预设

--- src/main/java/cc/moecraft/logger/coloring/GradientPresets.java
+++ src/main/java/cc/moecraft/logger/coloring/GradientPresets.java
@@ -12,4 +12,19 @@ import static cc.moecraft.logger.coloring.MultiPointLinearGradient.*;
  */
 public class GradientPresets
 {
+    public static final MultiPointLinearGradient RAINBOW;
+
+    static
+    {
+        // 这个彩虹预设的比例真的不是瞎打出来的... 自然的彩虹就是这样的比例...
+        RAINBOW = new MultiPointLinearGradient(
+                new Color(255, 0,   0  ),
+                new GradientPoint(new Color(255, 0,   255), 15),
+                new GradientPoint(new Color(0,   0,   255), 19),
+                new GradientPoint(new Color(0,   255, 255), 15),
+                new GradientPoint(new Color(0,   255, 0  ), 18),
+                new GradientPoint(new Color(255, 255, 0  ), 17),
+                new GradientPoint(new Color(255, 0,   0  ), 16)
+        );
+    }
 }
