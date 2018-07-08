354128798c821ea8d5bce805650a357d7678d08f
Hykilpikonna <admin@moecraft.cc>
2018-07-08 15:03:16 +0800

    [+] 添加多点线性渐变类

---
+++ src/main/java/cc/moecraft/logger/coloring/MultiPointLinearGradient.java
@@ -0,0 +1,17 @@
+package cc.moecraft.logger.coloring;
+
+import lombok.Data;
+
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
+/**
+ * 此类由 Hykilpikonna 在 18-7-7 创建!
+ * Created by Hykilpikonna on 18-7-7!
+ * Twitter: @Hykilpikonna
+ * QQ/Wechat: 871674895
+ */
+public class MultiPointLinearGradient
+{
+}
