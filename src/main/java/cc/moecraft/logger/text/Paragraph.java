3f9b2bea6d1ab7498f27e818cc21cb83275fa882
Hykilpikonna <admin@moecraft.cc>
2018-07-09 22:44:35 +0800

    [+] 添加文字块(Paragraph)类

---
+++ src/main/java/cc/moecraft/logger/text/Paragraph.java
@@ -0,0 +1,18 @@
+package cc.moecraft.logger.text;
+
+import cc.moecraft.logger.HyLogger;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * 此类由 Hykilpikonna 在 18-7-8 创建!
+ * Created by Hykilpikonna on 18-7-8!
+ * Twitter: @Hykilpikonna
+ * QQ/Wechat: 871674895
+ */
+public class Paragraph
+{
+}
