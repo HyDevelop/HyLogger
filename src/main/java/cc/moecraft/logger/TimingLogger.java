54cf095e76920a77205f079d61b5a13dfd89c1f1
Hykilpikonna <admin@moecraft.cc>
2018-07-10 08:52:44 +0800

    [+] TimingLogger类

---
+++ src/main/java/cc/moecraft/logger/TimingLogger.java
@@ -0,0 +1,14 @@
+package cc.moecraft.logger;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/07/10 创建!
+ * Created by Hykilpikonna on 2018/07/10!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class TimingLogger
+{
+    private long startingTime;
+}
