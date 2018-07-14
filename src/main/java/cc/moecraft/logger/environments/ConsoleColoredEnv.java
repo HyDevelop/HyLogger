4c20eea3ce6ebc81b70aac449b13f1b0e5346efd
Hykilpikonna <admin@moecraft.cc>
2018-07-14 15:58:41 +0800

    [+] 实现PRESET_ONLY支持级别

--- src/main/java/cc/moecraft/logger/environments/ConsoleColoredEnv.java
+++ src/main/java/cc/moecraft/logger/environments/ConsoleColoredEnv.java
@@ -4,6 +4,7 @@ import org.fusesource.jansi.AnsiConsole;
 
 import static cc.moecraft.logger.environments.ColorSupportLevel.FORCED;
 import static cc.moecraft.logger.environments.ColorSupportLevel.PASSTHROUGH;
+import static cc.moecraft.logger.environments.ColorSupportLevel.PRESET_ONLY;
 
 /**
  * 此类由 Hykilpikonna 在 2018/07/03 创建!
@@ -26,8 +27,18 @@ public class ConsoleColoredEnv extends LogEnvironment
     @Override
     public void logRaw(String message)
     {
-        // TODO: PRESET_ONLY
         if (colorSupportLevel == FORCED) System.out.println(message);
+        else if (colorSupportLevel == PRESET_ONLY) AnsiConsole.out.println(removeRGB(message));
         else AnsiConsole.out.println(message);
+    }
+
+    /**
+     * 移除所有RGB同时保留所有预设
+     * @param original 源
+     * @return 替换后的
+     */
+    public String removeRGB(String original)
+    {
+        return original.replaceAll("\\033\\[38;2;.*?m", "");
     }
 }
