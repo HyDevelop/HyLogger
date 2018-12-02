814fd7132567b8e338a272a8294815b3f69181f1
Hykilpikonna <admin@moecraft.cc>
2018-12-02 07:29:39 -0500

    [+] Format utils

---
+++ src/main/java/cc/moecraft/logger/utils/FormatUtils.java
@@ -0,0 +1,13 @@
+package cc.moecraft.logger.utils;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/12/02 创建!
+ * Created by Hykilpikonna on 2018/12/02!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class FormatUtils
+{
+}
