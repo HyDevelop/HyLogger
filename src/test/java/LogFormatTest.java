a6dd690f641a3758e4afe109218d30368b97f1d6
Hykilpikonna <admin@moecraft.cc>
2018-12-02 07:32:38 -0500

    [T] Test formatting

---
+++ src/test/java/LogFormatTest.java
@@ -0,0 +1,26 @@
+import cc.moecraft.logger.HyLogger;
+import cc.moecraft.logger.LoggerInstanceManager;
+import cc.moecraft.logger.environments.ColorSupportLevel;
+import cc.moecraft.logger.environments.ConsoleColoredEnv;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/12/02 创建!
+ * Created by Hykilpikonna on 2018/12/02!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class LogFormatTest
+{
+    public static void main(String[] args)
+    {
+        LoggerInstanceManager lim = new LoggerInstanceManager(new ConsoleColoredEnv(ColorSupportLevel.FORCED));
+        HyLogger logger = lim.getLoggerInstance("FormatTest", true);
+
+        // Test obtained from:  https://blog.csdn.net/skyupward/article/details/54864522
+        logger.log("Set {1,2} differs from {}", 3);
+        logger.log("Set \\{} differs from {}", "3");
+        logger.log("File name is C:\\\\{}.", "file.zip");
+    }
+}
