42fb63141dc128fee55cc578331795b767f30528
Hykilpikonna <admin@moecraft.cc>
2018-12-16 17:40:03 -0500

    [+] Color modes enum

---
+++ src/main/java/cc/moecraft/logger/coloring/AnsiColorMode.java
@@ -0,0 +1,22 @@
+package cc.moecraft.logger.coloring;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/12/16 创建!
+ * Created by Hykilpikonna on 2018/12/16!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+@AllArgsConstructor
+@Getter
+public enum AnsiColorMode
+{
+    COLOR_RGB("2;%s;%s;%s"),
+    COLOR_8BIT("5;%s");
+    
+    private final String format;
+}
