28dbbf62d84f1427ec4d473222e538608375aaa8
Hykilpikonna <admin@moecraft.cc>
2018-12-17 20:49:37 -0500

    [T] Test color conversion

---
+++ src/test/java/color/ColorConverterTest.java
@@ -0,0 +1,56 @@
+package color;
+
+import cc.moecraft.logger.utils.ColorConverter;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/12/16 创建!
+ * Created by Hykilpikonna on 2018/12/16!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class ColorConverterTest
+{
+    public static void main(String[] args)
+    {
+        for (int r = 0; r < 256; r += 16)
+        {
+            for (int g = 0; g < 256; g += 16)
+            {
+                for (int b = 0; b < 256; b += 16)
+                {
+                    System.out.print(ColorConverter.rgbTo8Bit(r, g, b) + "|");
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        
+        for (int r = 0; r < 256; r += 16)
+        {
+            for (int g = 0; g < 256; g += 16)
+            {
+                for (int b = 0; b < 256; b += 16)
+                {
+                    System.out.print("\033[38;5;" + ColorConverter.rgbTo8Bit(r, g, b) + "m█");
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        
+        for (int r = 0; r < 256; r += 16)
+        {
+            for (int g = 0; g < 256; g += 16)
+            {
+                for (int b = 0; b < 256; b += 16)
+                {
+                    System.out.print("\033[38;2;" + r + ";" + g + ";" + b + "m█");
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
