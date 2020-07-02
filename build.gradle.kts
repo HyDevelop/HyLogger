21f89a13bd3ca9ff837a3fd8c305b6928c96b1de
Hykilpikonna <admin@moecraft.cc>
2020-07-01 23:22:31 -0400

    [+] Re-initialize as gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    java
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "org.hydev"
+version = "2.0.0.255"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testCompile("junit", "junit", "4.12")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
