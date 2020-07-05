de64673c7b005e10a8bd23341db43449742bca52
Hykilpikonna <admin@moecraft.cc>
2020-07-05 18:48:59 -0400

    [O] Remove save on close prompt

--- src/main/kotlin/org/hydev/logger/environments/FileEnv.kt
+++ src/main/kotlin/org/hydev/logger/environments/FileEnv.kt
@@ -1,22 +1,12 @@
 package org.hydev.logger.environments
 
-import org.fusesource.jansi.AnsiConsole
 import org.hydev.logger.HyLoggerConfig.fileFormat
 import org.hydev.logger.HyLoggerConfig.fileTimePattern
-import org.hydev.logger.format.AnsiColor
 import org.hydev.logger.now
 import org.hydev.logger.withoutFormat
 import java.io.File
 import java.io.PrintWriter
 
-/**
- * 此类由 Hykilpikonna 在 2018/05/27 创建!
- * Created by Hykilpikonna on 2018/05/27!
- * Github: https://github.com/hykilpikonna
- * QQ: admin@moecraft.cc -OR- 871674895
- *
- * @author Hykilpikonna
- */
 open class FileEnv(val file: File) : LogEnvironment()
 {
     var fileWriter: PrintWriter
@@ -27,8 +17,8 @@ open class FileEnv(val file: File) : LogEnvironment()
         if (!file.exists()) file.createNewFile()
         fileWriter = file.printWriter()
 
+        // Save on close
         Runtime.getRuntime().addShutdownHook(Thread {
-            AnsiConsole.out.println(AnsiColor.GREEN.toString() + "检测到进程关闭... 正在保存Log...")
             fileWriter.flush()
             fileWriter.close()
         })
