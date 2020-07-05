694990c1a6c23d9cf8e8d6b50668581ae485a3e5
Hykilpikonna <admin@moecraft.cc>
2020-07-05 18:39:49 -0400

    [O] Refactor ColorSupportLevel

--- src/main/kotlin/org/hydev/logger/environments/ConsoleEnv.kt
+++ src/main/kotlin/org/hydev/logger/environments/ConsoleEnv.kt
@@ -2,18 +2,7 @@ package org.hydev.logger.environments
 
 import org.hydev.logger.withoutFormat
 
-/**
- * 此类由 Hykilpikonna 在 2018/07/03 创建!
- * Created by Hykilpikonna on 2018/07/03!
- * Github: https://github.com/hykilpikonna
- * QQ: admin@moecraft.cc -OR- 871674895
- *
- * @author Hykilpikonna
- */
 class ConsoleEnv: LogEnvironment()
 {
-    override fun logRaw(message: String)
-    {
-        println(message.withoutFormat())
-    }
+    override fun logRaw(message: String) = println(message.withoutFormat())
 }
