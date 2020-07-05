9d47dcff14be35d0b67fcb184c6626b436f44abe
Hykilpikonna <admin@moecraft.cc>
2020-07-05 19:09:01 -0400

    [+] Refactor TimingLogger

--- src/main/kotlin/org/hydev/logger/TimingLogger.kt
+++ src/main/kotlin/org/hydev/logger/TimingLogger.kt
@@ -1,57 +1,12 @@
 package org.hydev.logger
 
-/**
- * 此类由 Hykilpikonna 在 2018/07/10 创建!
- * Created by Hykilpikonna on 2018/07/10!
- * Github: https://github.com/hykilpikonna
- * Meow!
- *
- * @author Hykilpikonna
- */
 class TimingLogger(private val logger: HyLogger)
 {
-    private var startingTime: Long? = null
-    fun time(): TimingLogger
-    {
-        logger.log(milliseconds.toString() + "ms")
-        return this
-    }
+    var startTime: Long = 0
 
-    fun timeAndReset(): TimingLogger
-    {
-        return time().reset()
-    }
+    val elapsed: Long
+        get() = System.currentTimeMillis() - startTime
 
-    fun timeNano(): TimingLogger
-    {
-        logger.log("$time nanos")
-        return this
-    }
-
-    fun timeNanoAndReset(): TimingLogger
-    {
-        return timeNano().reset()
-    }
-
-    val time: Long
-        get() = System.nanoTime() - startingTime!!
-
-    val milliseconds: Double
-        get() = time.toDouble() / 1000000.0
-
-    fun reset(): TimingLogger
-    {
-        startingTime = System.nanoTime()
-        return this
-    }
-
-    fun init(): TimingLogger
-    {
-        return reset()
-    }
-
-    fun clear()
-    {
-        startingTime = null
-    }
+    fun time() = apply { logger.log("$elapsed ms") }
+    fun reset() = apply { startTime = System.currentTimeMillis() }
 }
