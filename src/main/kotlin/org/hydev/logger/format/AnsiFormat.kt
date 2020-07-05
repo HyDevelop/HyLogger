49338877afeac40feca46f4621e170e2d733d754
Hykilpikonna <admin@moecraft.cc>
2020-07-05 18:09:49 -0400

    [O] Optimize colors and formats implementation

--- src/main/kotlin/org/hydev/logger/format/AnsiFormat.kt
+++ src/main/kotlin/org/hydev/logger/format/AnsiFormat.kt
@@ -1,5 +1,8 @@
 package org.hydev.logger.format
 
+import org.hydev.logger.format.AnsiConstants.ESC_PREFIX
+import org.hydev.logger.format.AnsiConstants.SUFFIX
+
 /**
  * 此类由 Hykilpikonna 在 2018/05/04 创建!
  * Created by Hykilpikonna on 2018/05/04!
@@ -8,37 +11,18 @@ package org.hydev.logger.format
  *
  * @author Hykilpikonna
  */
-enum class AnsiFormat(var code: String, vararg placeholders: String)
+enum class AnsiFormat(var code: Int, vararg val placeholders: Char)
 {
-    RESET("0"), HIGH_INTENSITY("1", "l"), LOW_INTENSITY("2"), ITALIC("3", "o"), UNDERLINE(
-    "4",
-    "n"
-),
-    BLINK("5"), RAPID_BLINK("6"), REVERSE_VIDEO("7"), INVISIBLE_TEXT("8");
+    RESET(0),
+    HIGH_INTENSITY(1, 'l'),
+    LOW_INTENSITY(2),
+    ITALIC(3, 'o'),
+    UNDERLINE(4, 'n'),
+    BLINK(5),
+    RAPID_BLINK(6),
+    REVERSE_VIDEO(7),
+    INVISIBLE_TEXT(8);
 
-    override fun toString(): String
-    {
-        return AnsiConstants.ESC_PREFIX + code + AnsiConstants.SUFFIX
-    }
-
-    companion object
-    {
-        fun replaceAllFormatWithANSI(original: String): String
-        {
-            val result = arrayOf(original)
-            AnsiConstants.formatsPlaceholderIndex.forEach { (k: String?, v: AnsiFormat?) ->
-                result[0] = result[0].replace(AnsiConstants.FORMAT_PREFIX + k, v.toString())
-            }
-            AnsiConstants.colorsPlaceholderIndex.forEach { (k: String?, v: AnsiColor?) ->
-                result[0] = result[0].replace(AnsiConstants.FORMAT_PREFIX + k, v.toString())
-            }
-            return result[0]
-        }
-    }
-
-    init
-    {
-        AnsiConstants.formats.add(this)
-        for (placeholder in placeholders) AnsiConstants.formatsPlaceholderIndex[placeholder] = this
-    }
+    override fun toString() = value
+    val value = ESC_PREFIX + code + SUFFIX
 }
