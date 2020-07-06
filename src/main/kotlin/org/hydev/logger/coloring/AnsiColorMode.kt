84c5b12e62e701c4286a0e2a5cc4b4e40a38df5d
Hykilpikonna <admin@moecraft.cc>
2020-07-05 21:18:26 -0400

    [-] Remove unnecessary stuff

--- src/main/kotlin/org/hydev/logger/coloring/AnsiColorMode.kt
+++ src/main/kotlin/org/hydev/logger/coloring/AnsiColorMode.kt
@@ -8,9 +8,4 @@ enum class AnsiColorMode(val format: (Color) -> String)
 {
     COLOR_RGB({ it.toAnsi256() }),
     COLOR_8BIT({ it.toAnsi8() });
-
-    /**
-     * Format a color into this color format with r, g, and b params.
-     */
-    fun format(r: Int, g: Int, b: Int) = format(Color(r, g, b))
 }
--- src/main/kotlin/org/hydev/logger/coloring/AnsiRGB.kt
+++ src/main/kotlin/org/hydev/logger/coloring/AnsiRGB.kt
@@ -1,7 +1,10 @@
 package org.hydev.logger.coloring
 
 import org.hydev.logger.HyLoggerConfig.colorMode
-import org.hydev.logger.format.AnsiConstants
+import org.hydev.logger.format.AnsiConstants.BACKGROUND
+import org.hydev.logger.format.AnsiConstants.ESC_PREFIX
+import org.hydev.logger.format.AnsiConstants.FOREGROUND
+import org.hydev.logger.format.AnsiConstants.SUFFIX
 import java.awt.Color
 
 class AnsiRGB(var foreground: Color, var background: Color? = null)
@@ -13,8 +16,8 @@ class AnsiRGB(var foreground: Color, var background: Color? = null)
      */
     override fun toString(): String
     {
-        var result = AnsiConstants.ESC_PREFIX + AnsiConstants.FOREGROUND + colorMode.format(foreground)
-        if (background != null) result += AnsiConstants.BACKGROUND + colorMode.format(background!!)
-        return result + AnsiConstants.SUFFIX
+        var result = ESC_PREFIX + FOREGROUND + colorMode.format(foreground)
+        if (background != null) result += BACKGROUND + colorMode.format(background!!)
+        return result + SUFFIX
     }
 }
