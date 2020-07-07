7c26d6a65f7d3d3a0db18881f11d67b42d3ce8c9
Hykilpikonna <admin@moecraft.cc>
2020-07-07 15:32:18 -0400

    [O] Make BlueTest outputs look better

--- src/test/java/BlueTest.kt
+++ src/test/java/BlueTest.kt
@@ -1,3 +1,10 @@
+
+import org.hydev.logger.background
+import org.hydev.logger.foreground
+import org.hydev.logger.format.AnsiColor.RESET
+import java.awt.Color
+import java.awt.Color.LIGHT_GRAY
+
 /**
  * 此类由 Hykilpikonna 在 2018/05/04 创建!
  * Created by Hykilpikonna on 2018/05/04!
@@ -11,11 +18,11 @@ object BlueTest
     @JvmStatic
     fun main(args: Array<String>)
     {
-        var i = 0
-        while (i < 256)
+        for (b in 0..255)
         {
-            print(String.format(" \u001b[38;2;0;0;%sm %s \u001b[0m ", i, i))
-            i += 1
+            if (b % 16 == 0) println()
+
+            print(LIGHT_GRAY.foreground() + Color(0,0,b).background() + " " + b.toString().padStart(3, ' ') + " " + RESET + " ")
         }
     }
 }
