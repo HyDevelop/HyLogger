ad4eed2f93c432372b43e773dc3cc1f97c6a2cbb
Hykilpikonna <admin@moecraft.cc>
2020-07-07 21:57:49 -0400

    [+] Rebuild MultiPointLinearGradient

---
+++ src/main/kotlin/org/hydev/logger/coloring/GradientPoint.kt
@@ -0,0 +1,13 @@
+package org.hydev.logger.coloring
+
+import java.awt.Color
+
+/**
+ * A gradient point on a line with color and position
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2020-07-07 16:33
+ */
+class GradientPoint(val color: Color, val pos: Int)
