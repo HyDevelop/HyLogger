385b01fe36fb2c009ceeeec4b2e4f835c8d6a505
Hykilpikonna <admin@moecraft.cc>
2020-07-10 22:06:44 -0400

    [U] Update tests

--- src/test/java/LoggerTest.kt
+++ src/test/java/LoggerTest.kt
@@ -1,5 +1,6 @@
 
 import org.hydev.logger.HyLogger
+import org.hydev.logger.HyLoggerConfig
 import org.hydev.logger.coloring.GradientPresets.BOP
 import org.hydev.logger.coloring.GradientPresets.BPR
 import org.hydev.logger.coloring.GradientPresets.RAINBOW
@@ -20,44 +21,55 @@ object LoggerTest
     {
         val logger = HyLogger("LoggerTest")
         logger.timing.reset()
+
         logger.log("一条测试Log消息")
-        logger.debug("一条测试Debug消息")
         logger.error("一条测试Error消息")
         logger.warning("一条测试Warning消息")
+        logger.debug("一条没发出去的Debug消息")
+        HyLoggerConfig.debug = true
+        logger.debug("一条发出去了的Debug消息")
         logger.timing.time().reset()
+
         logger.fancy.gradient("测试渐变从深蓝到浅蓝", Color.BLUE, Color.CYAN)
         logger.timing.time().reset()
+
         logger.fancy.gradient("测试渐变从橘色到浅蓝", Color.ORANGE, Color.CYAN)
         logger.timing.time().reset()
+
         logger.fancy.gradient(
             "测试黄绿渐变到天蓝",
             Color(0, 242, 96),
             Color(80, 161, 230)
         )
         logger.timing.time().reset()
+
         logger.fancy.gradient(
             "测试橙色渐变到粉色",
             Color(255, 140, 0),
             Color(255, 0, 128)
         )
         logger.timing.time().reset()
+
         logger.fancy.gradient("""
             |############################################
-            |##############测试彩虹多点渐变预设##############
+            |########## 中文不会打断渐变色啦w ###########
             |############################################""".trimMargin(), RAINBOW)
         logger.timing.time().reset()
 
-        logger.fancy.gradient("##############测试蓝到紫到红多点渐变##############", BPR)
+        logger.fancy.gradient("""
+            |############################################
+            |########### Blue to Purple to Red ##########
+            |############################################""".trimMargin(), BPR)
         logger.timing.time().reset()
 
-        logger.log("测试Paragraph斜向线性渐变 #1:")
+        logger.log("Testing angled gradient on text block #1:")
         logger.fancy.gradient("""
             ┬ ┬┬ ┬┬  ┌─┐┌─┐┌─┐┌─┐┬─┐
             ├─┤└┬┘│  │ ││ ┬│ ┬├┤ ├┬┘
             ┴ ┴ ┴ ┴─┘└─┘└─┘└─┘└─┘┴└─""".trimIndent(), BPR, 15.0)
         logger.timing.time().reset()
 
-        logger.log("测试Paragraph斜向线性渐变 #2:")
+        logger.log("Testing angled gradient on text block #2:")
         logger.fancy.gradient("""
                 __  __      __                               
                / / / /_  __/ /   ____  ____ _____ ____  _____
