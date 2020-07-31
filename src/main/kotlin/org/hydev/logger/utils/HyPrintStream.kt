2f0ecd44a60df35df2337fc76ba44b1a90957369
Hykilpikonna <admin@moecraft.cc>
2020-07-31 16:38:16 -0400

    [O] Decouple HyPrintStream with HyLogger

--- src/main/kotlin/org/hydev/logger/utils/HyPrintStream.kt
+++ src/main/kotlin/org/hydev/logger/utils/HyPrintStream.kt
@@ -1,6 +1,5 @@
 package org.hydev.logger.utils
 
-import org.hydev.logger.HyLogger
 import java.io.OutputStream
 import java.io.PrintStream
 
@@ -12,7 +11,7 @@ import java.io.PrintStream
  * @author Vanilla (https://github.com/VergeDX)
  * @since 2020-07-13 22:05
  */
-class HyPrintStream(val original: PrintStream, val log: (Any) -> Unit = { HyLogger.general.log(it) })
+class HyPrintStream(val original: PrintStream, val log: (Any) -> Unit)
     : PrintStream(object: OutputStream() { override fun write(b: Int) {}})
 {
     override fun checkError() = super.checkError() || original.checkError()
